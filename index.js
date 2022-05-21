e2b6b330cb7abaa3766609609aecb9efc46cc448
millandavid <71908868+millandavid@users.noreply.github.com>
2022-05-21 01:07:49 -0500

    Created basic command handlers

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-const Discord = require("discord.js")
-require("dotenv").config() //load token
+const Discord = require('discord.js');
+require('dotenv').config(); //load token
 
 const client = new Discord.Client({
-  intents: [
-    "GUILDS",
-    "GUILD_MESSAGES"
-  ]
-})
+  intents: ['GUILDS', 'GUILD_MESSAGES'],
+});
 
+let bot = {
+  client,
+  prefix: 'n.',
+  owners: ['221673029657165824'],
+};
 
-client.on("ready", () =>{
-  console.log(`Logged in as ${client.user.tag}`)
-})
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
 
-client.on("messageCreate",(message) => {
-  if(message.content == "hi"){
-    message.reply("Hello Milly")
-  }
-})
+client.loadEvents = (bot, reload) => require('./handlers/events')(bot, reload);
+client.loadCommands = (bot, reload) =>
+  require('./handlers/commands')(bot, reload);
 
-client.login(process.env.TOKEN)
+client.loadEvents(bot, false);
+client.loadCommands(bot, false);
+
+module.exports = bot;
+
+// client.on("ready", () =>{
+//   console.log(`Logged in as ${client.user.tag}`)
+// })
+
+// client.on("messageCreate",(message) => {
+//   if(message.content == "hi"){
+//     message.reply("Hello Milly")
+//   }
+// })
+
+client.login(process.env.TOKEN);
